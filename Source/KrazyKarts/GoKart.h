1bf56b142f58741b72b1460bda6d41be444dfbd1
babo24128 <babo24128@naver.com>
2024-11-02 01:41:13 +0900

    쿼터니언을 사용한 회전

--- Source/KrazyKarts/GoKart.h
+++ Source/KrazyKarts/GoKart.h
@@ -24,10 +24,13 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
+	void ApplyRotation(float DeltaTime);
+
 	void UpdateLocationFromVelocity(float DeltaTime);
 
 	// 차의 중량
@@ -38,7 +41,13 @@ private:
 	UPROPERTY(EditAnywhere)
 	float MaxDrivingForce = 10000;
 
+	// full control throw 에서 초당 회전한 도 수(degrees/s)
+	UPROPERTY(EditAnywhere)
+	float MaxDegreesPerSecond = 90;
+
 	void MoveForward(float Value);
+	void MoveRight(float Value);
 	FVector Velocity;
 	float Throttle;
+	float SteeringThrow;
 };
